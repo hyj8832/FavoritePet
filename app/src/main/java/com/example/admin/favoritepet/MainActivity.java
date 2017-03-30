ad6f7212479aeb227f00c4683f4cc6214f375fae
hyj8832 <hyj8832@naver.com>
2017-03-30 20:43:44 +0900

    #3 CheckBox Event Programming

--- app/src/main/java/com/example/admin/favoritepet/MainActivity.java
+++ app/src/main/java/com/example/admin/favoritepet/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.admin.favoritepet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+     CheckBox checkSelect;
+    TextView textQuest;
+     RadioGroup rg;
+    RadioButton radiosichu,radiorabbit,radiocat;
+    Button butOk;
+    ImageView imgvPet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkSelect=(CheckBox)findViewById(R.id.check_select);
+        textQuest=(TextView)findViewById(R.id.text_quest); 
+        rg=(RadioGroup) findViewById(R.id.rg);
+        radiosichu=(RadioButton) findViewById(R.id.radio_dog);
+        radiocat=(RadioButton) findViewById(R.id.radio_cat);
+        radiorabbit=(RadioButton) findViewById(R.id.radio_rabbit);
+        butOk=(Button)findViewById(R.id.but_ok);
+        imgvPet=(ImageView)findViewById(R.id.imgv_pet);
+        checkSelect.setOnCheckedChangeListener(this);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if(checkSelect.isChecked()){
+                textQuest.setVisibility(View.VISIBLE);
+                rg.setVisibility(View.VISIBLE);
+                butOk.setVisibility(View.VISIBLE);
+                imgvPet.setVisibility(View.VISIBLE);
+            }
+        else{
+                textQuest.setVisibility(View.INVISIBLE);
+                rg.setVisibility(View.INVISIBLE);
+                butOk.setVisibility(View.INVISIBLE);
+                imgvPet.setVisibility(View.INVISIBLE);
+            }
     }
 }
